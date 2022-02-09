17c72128071c46b513df659fadb78c6bcefcf6cf
Atharv Vikhe <atharvvikhe@gmail.com>
2022-02-09 17:09:33 +0530

    Add section Done

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDo List Website
+
+This Website make a ToDo List using System Local-Storage. This website have 2 Section Registration and ToDo-List Display. You can perform all CURD Operation on the list.
---
+++ js/main.js
@@ -0,0 +1,235 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getDatabase, ref, set, child, get, onValue } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDjT20uemg2o3-vO-G3226x1POI0SsGJgU",
+    authDomain: "sample-project-ae235.firebaseapp.com",
+    databaseURL: "https://sample-project-ae235-default-rtdb.firebaseio.com",
+    projectId: "sample-project-ae235",
+    storageBucket: "sample-project-ae235.appspot.com",
+    messagingSenderId: "553746740022",
+    appId: "1:553746740022:web:add969c9303dc070267997",
+    measurementId: "G-TV840FF717"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+let id = 0
+
+// Function for Check Validation in Registration Form
+document.getElementById('registerUser').addEventListener('click', (e) => {
+    e.preventDefault();
+    // Access the Inputed Value
+    let iemail = document.getElementById('email').value;
+    let ipwd = document.getElementById('pwd').value;
+    let iconpwd = document.getElementById('conpwd').value;
+    let iname = document.getElementById('name').value;
+    //let iuid = document.getElementById('uid').value;
+
+    let ename = "", eemail = "", epwd = "", ecompwd = "";
+    let ncn = 0, ecn = 0, pcn = 0, cpcn = 0, mcn = 0;
+
+    // Check Validation for blank Field
+    if (iname == "" && iemail == "" && ipwd == "" && iconpwd == "") {
+        document.getElementById('error-name').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        document.getElementById('error-email').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        document.getElementById('error-pwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        document.getElementById('error-conpwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        return 0;
+    }
+
+    if (iname == "") {
+        document.getElementById('error-name').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+    }
+    if (iname != "") {
+        document.getElementById('error-name').innerHTML = "";
+        mcn++;
+    }
+
+    // Check for email field
+    let regexForEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+    if (iemail == "") {
+        document.getElementById('error-email').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        ecn = 1;
+    }
+    if (regexForEmail.test(iemail) != true && ecn != 1) {
+        document.getElementById('error-email').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Enter vaild Email ID</div>';
+    }
+    if (regexForEmail.test(iemail) && ecn != 1) {
+        document.getElementById('error-email').innerHTML = "";
+        mcn++;
+    }
+
+    // Validtion for Password
+    let regexForCaps = /[A-Z]/g;
+    let regexForNumber = /[0-9]/g;
+    let regexForSpeChar = /\W/g;
+
+    if (ipwd == "") {
+        document.getElementById('error-pwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        pcn = 1;
+    }
+
+    if (ipwd.length < 8 && pcn != 1) {
+        epwd += "Password must have 8 Char.<br>";
+        pcn = 2;
+    }
+    if (regexForCaps.test(ipwd) != true && pcn != 1) {
+        epwd += "Password must have 1 Capital Letter.<br>";
+        pcn = 2;
+    }
+    if (regexForNumber.test(ipwd) != true && pcn != 1) {
+        epwd += "password must have 1 Number.<br>";
+        pcn = 2;
+    }
+    if (regexForSpeChar.test(ipwd) != true && pcn != 1) {
+        epwd += "Password must have 1 Special Char.<br>";
+        pcn = 2;
+    }
+    if (pcn == 2) {
+        document.getElementById('error-pwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">' + epwd + '</div>';
+    }
+
+    if (pcn != 1 && pcn != 2) {
+        document.getElementById('error-pwd').innerHTML = "";
+        mcn++;
+    }
+
+    // Validation for Confirm Password
+    if (iconpwd == "") {
+        document.getElementById('error-conpwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Please fill this field</div>';
+        cpcn = 1;
+    }
+    if (iconpwd != ipwd && cpcn != 1) {
+        document.getElementById('error-conpwd').innerHTML = '<div class="alert alert-danger errorshow" role="alert">Confirm Password must match with Password</div>';
+    }
+    if (iconpwd == ipwd && cpcn != 1) {
+        document.getElementById('error-conpwd').innerHTML = "";
+        mcn++;
+    }
+    if (mcn != 4) return 0;
+
+    // If All Validation Checked
+    document.getElementById('input-name').innerText = iname;
+    document.getElementById('regi').style.display = "none";
+    document.getElementById('todos').style.display = "block";
+
+});
+
+let taskArr = [];
+let maxNo = 0;
+// const dbref = ref(database);
+// get(child(dbref, 'task')).then((snapshot) => {
+//     taskArr = snapshot.val();
+//     for (let index = 0; index < taskArr.length; index++) {
+//         display_list(index, taskArr[index].title, taskArr[index].task);
+//     }
+//     console.log(taskArr.length);
+// });
+//
+const dbrefrt = ref(database, 'task');
+onValue(dbrefrt, (snapshot) => {
+    let arr = [];
+    document.getElementById('todo-list').innerHTML = "";
+    snapshot.forEach(childss => {
+        display_list(childss.val().id,childss.val().title,childss.val().task); 
+        if(maxNo<childss.val().id) {
+            maxNo = childss.val().id;
+        }      
+    });
+});
+
+// Adding new item in list and displaying
+document.getElementById('addUserTask').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('tno').value = maxNo + 1;
+    const num = maxNo + 1;
+    const titles = document.getElementById('titlea').value;
+    const dec = document.getElementById('desca').value;
+    if (num != "" && titles != "" && dec != "") {
+        document.getElementById('titlea').value = "";
+        document.getElementById('desca').value = "";
+        set(ref(database, 'task/' + num), {
+            id:num,
+            title: titles,
+            task: dec
+        }).then(() => {
+            console.log("Task Added!!!");
+        }).catch((error) => {
+            console.log(error);
+        });
+    } else {
+        return 0;
+    }
+});
+
+
+// Display Item in List
+function display_list(a, b, c) {
+    let ele = document.getElementById('todo-list');
+    const tempele = document.createElement('li');
+
+    ele.appendChild(tempele);
+
+    let lc = ele.lastChild;
+    lc.id = "item" + a;
+    let cl = ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'list-item']
+    lc.classList.add(...cl);
+    lc.innerHTML = '<div class="ms-2 me-auto" style="max-width: 550px;"><span id="tid">' + a + '</span><div class="fw-bold fs-5" id="title' + a + '">' + b + '</div><span id="desc' + a + '">' + c + '</span></div><div class="badge rounded-pill fs-5 list-btn hov" data-bs-toggle="modal" data-bs-target="#edit-item" onclick="display_edit(' + a + ')"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></div><div class="badge rounded-pill fs-5 ms-3 list-btn hov" data-bs-toggle="modal" data-bs-target="#delete-item" onclick="display_delete(' + a + ')"><i class="fa fa-trash-o" aria-hidden="true"></i></div>';
+}
+
+// Editing exsiting Item in list and displaying
+function display_edit(id) {
+    document.getElementById('ide').value = id
+    document.getElementById('titlee').value = myToDoList[id].title;
+    document.getElementById('desce').value = myToDoList[id].desc;
+}
+
+function editItem() {
+    const titles = document.getElementById('titlee').value;
+    const dec = document.getElementById('desce').value;
+    const id = document.getElementById('ide').value;
+    if (titles != "" && dec != "") {
+        myToDoList[id].title = titles;
+        myToDoList[id].desc = dec;
+
+        localStorage.setItem('ToDo-storage', JSON.stringify(myToDoList));
+
+        document.getElementById('title' + id).innerText = titles;
+        document.getElementById('desc' + id).innerText = dec;
+    } else {
+        return 0;
+    }
+
+}
+
+// Deleting exsiting Item from list and removing from display
+function display_delete(id) {
+    document.getElementById('idd').value = id;
+}
+
+function deleteItem() {
+    const id = Number.parseInt(document.getElementById('idd').value);
+
+    let tempArr1 = myToDoList.slice(0, id);
+    let tempArr2 = myToDoList.slice(id + 1, myToDoList.length);
+
+    myToDoList = tempArr1.concat(tempArr2);
+
+    localStorage.setItem('ToDo-storage', JSON.stringify(myToDoList));
+
+    if (myToDoList.length == 0) {
+        document.getElementById('empty-list').style.display = "block";
+    }
+
+    showadel(myToDoList);
+}
